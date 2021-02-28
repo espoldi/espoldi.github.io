39f52d430dbf6a690a0a508832af99d3a1eddeaa
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-02-27 19:49:38 -0500

    changed side nav to contain about me section

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+    // Side Navbar Activation
     const slideMenu = document.querySelectorAll(".sidenav");
     M.Sidenav.init(slideMenu, {});
+
+    // Side Navbar Collapsible Activation
+    var sideCollapse = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(sideCollapse, {});
+    
 });
